bd782a079320234230ef38c220fd9f6a276862b5
John Rene Morales <156500586+pdzeus360@users.noreply.github.com>
2024-06-11 21:27:10 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/takeapp/ForgetPasswordActivity.java
@@ -0,0 +1,41 @@
+package com.example.takeapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_forget_password);
+        Button SendCode = findViewById(R.id.sendCode);
+
+        TextView codeMsg = findViewById(R.id.codeSendMessage);
+
+        EditText codeInput = findViewById(R.id.codeInputText);
+
+        TextView codeReqMsg = findViewById(R.id.codeReqMessage);
+
+        Button resendcode = findViewById(R.id.resendCode);
+
+        Button confirm = findViewById(R.id.confirm);
+
+        codeMsg.setVisibility(View.INVISIBLE);
+        codeInput.setVisibility(View.INVISIBLE);
+        codeReqMsg.setVisibility(View.INVISIBLE);
+        resendcode.setVisibility(View.INVISIBLE);
+        confirm.setVisibility(View.INVISIBLE);
+
+    }
+}
