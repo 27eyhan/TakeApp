f6867ca2ac75743d34ac17b8ad00575b6e9705b0
John Rene Morales <156500586+pdzeus360@users.noreply.github.com>
2024-06-13 00:14:41 +0800

    Update ChangePassword.java

--- app/src/main/java/com/example/takeapp/ChangePassword.java
+++ app/src/main/java/com/example/takeapp/ChangePassword.java
@@ -1,13 +1,16 @@
 package com.example.takeapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,6 +18,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ChangePassword extends AppCompatActivity {
 
+    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +30,29 @@ public class ChangePassword extends AppCompatActivity {
         EditText confirmNewPass = findViewById(R.id.ConfirmnewPass);
         Button changePassButton = findViewById(R.id.changePassBtn);
 
+        dbHelper = new DatabaseHelper(this);
+
+
         changePassButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String NewPassword = newPass.getText().toString().trim();
                 String confirmNewPassword = confirmNewPass.getText().toString().trim();
+                String email = getIntent().getStringExtra("email");
+
+                TextView viewEmail = findViewById(R.id.showUserEmail);
+                viewEmail.setText(email);
+
                 if(NewPassword.isEmpty() || confirmNewPassword.isEmpty()) {
                     Toast.makeText(ChangePassword.this, "Please Enter Your New Password", Toast.LENGTH_SHORT).show();
-                }else if(confirmNewPassword.equals(NewPassword)){
-                    Toast.makeText(ChangePassword.this,"Your Password Successfully Change!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(ChangePassword.this, LoginActivity.class);
-                    startActivity(intent);
-                }else if(confirmNewPassword != NewPassword){
+                }else if(!confirmNewPassword.equals(NewPassword)){
                     Toast.makeText(ChangePassword.this,"Your Password do not match!", Toast.LENGTH_SHORT).show();
                 }else{
-                    Toast.makeText(ChangePassword.this,"Error: Please Try Again!", Toast.LENGTH_SHORT).show();
+                    dbHelper.updatePassword(email, NewPassword);
+                    Toast.makeText(ChangePassword.this,"Successfully Change Your Password", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(ChangePassword.this, LoginActivity.class);
+                    startActivity(intent);
                 }
             }
         });
